499dfd10e1f67cc811e92f5bec77d5fc5faed277
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-15 08:49:45 +0200

    refactor(constants): update constants variable into one constant, add search property

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,10 +1,17 @@
 import { Op } from 'sequelize';
+import { NoteFilterType } from './types';
 
-export const DEFAULT_LIMIT = 20;
-
-export const DEFAULT_OFFSET = 0;
-
-export const DEFAULT_CONDITION = 'all';
+export const DEFAULT_GET_VALUES: {
+  LIMIT: number;
+  OFFSET: number;
+  CONDITION: NoteFilterType;
+  SEARCH: string;
+} = {
+  LIMIT: 20,
+  OFFSET: 0,
+  CONDITION: 'all',
+  SEARCH: '',
+} as const;
 
 const LIMIT_DATE = new Date().setMonth(new Date().getMonth() - 3);
 
