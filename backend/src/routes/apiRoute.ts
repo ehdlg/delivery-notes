94cdcf16836e4aca445fa211006e554c658c29a7
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-10 12:05:06 +0200

    feat(routes): verify jwt token before accesing note routes

--- backend/src/routes/apiRoute.ts
+++ backend/src/routes/apiRoute.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import loginRoute from './loginRoute';
 import notesRoute from './notesRoute';
+import { verifyToken } from '../middlewares';
 
 const router = Router();
 
 router.use('/login', loginRoute);
 
-router.use('/', notesRoute);
+router.use('/', verifyToken, notesRoute);
 
 export default router;
