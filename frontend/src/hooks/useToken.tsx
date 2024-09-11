a5010b2d62d13fbe70b316bf1d8c3df042550d33
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-11 10:58:55 +0200

    feat(hooks): validate JWT locally for isLoggedIn check

--- frontend/src/hooks/useToken.tsx
+++ frontend/src/hooks/useToken.tsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { jwtDecode } from 'jwt-decode';
+import { DecodedToken } from '../types';
+import { TOKEN_ISSUER } from '../constants';
 
 export function useToken() {
   const initialValue: string | null = localStorage.getItem('token') ?? null;
@@ -16,7 +19,24 @@ export function useToken() {
     localStorage.setItem('token', newToken);
   };
 
-  const isLoggedIn = null != token;
+  const verifyToken = () => {
+    if (null == token) return false;
+
+    try {
+      const decoded = jwtDecode<DecodedToken>(token);
+      console.log(decoded, TOKEN_ISSUER);
+
+      console.log(decoded.iss == TOKEN_ISSUER);
+
+      return decoded.iss == TOKEN_ISSUER;
+    } catch (error) {
+      logout();
+
+      return false;
+    }
+  };
+
+  const isLoggedIn = null != token && verifyToken();
 
   return { token, login, logout, isLoggedIn };
 }
