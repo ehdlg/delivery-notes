2a167a6d7cd0d391559487ef2f3cf2b9bc518e37
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-11 10:59:27 +0200

    feat(hooks): add token to headers request in useNotes hooks

--- frontend/src/hooks/useNotes.tsx
+++ frontend/src/hooks/useNotes.tsx
@@ -1,12 +1,25 @@
 import useSwr from 'swr';
 import { FetchError } from '../errors';
 import { API_URL } from '../constants';
+import useToken from './useToken';
 
 function useNotes<T>(resource: string) {
   const URL = `${API_URL}${resource}`;
+  const { token } = useToken();
+
+  if (null == token)
+    throw new FetchError(
+      'An error occurred while fetching the data',
+      { error: 'No está autorizado' },
+      401
+    );
 
   const fetcher = async (url: string) => {
-    const res = await fetch(url);
+    const res = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
 
     if (!res.ok) {
       const info = await res.json();
