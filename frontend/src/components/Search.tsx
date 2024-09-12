6669d0694fec0808d3def1039ba0a99ca6aee435
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-12 10:30:19 +0200

    feat(styles): update search styles

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -19,12 +19,12 @@ function Search() {
   return (
     <form
       onSubmit={onSubmit}
-      className='flex rounded border bg-gray-50 focus-within:border-b-2 focus-within:border-gray-300'
+      className='flex rounded border bg-gray-50 focus-within:border-gray-200'
     >
       <input
         type='text'
         ref={inputRef}
-        className='[&::-webkit-inner-spin-button]:appearance-nonebg-inherit w-full rounded bg-inherit text-center text-2xl text-gray-700 outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none'
+        className='[&::-webkit-inner-spin-button]:appearance-nonebg-inherit w-full rounded bg-inherit text-center text-2xl text-gray-700 shadow-inner outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none'
       />
 
       <button type='submit'>
